f3736fab5a275bb2b3be8d7354cede76981ddd5f
Malik Gray <mikey@starfantasygames.com>
2016-07-22 05:12:34 -0400

    Both the TankPlayerController and TankAIController now nave an AimAt method that allows them to target a location which is inherited from the Tank class.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -36,8 +36,9 @@ void ATankPlayerController::AimTowardsCrossHair()
 	FVector HitLocation; // Out parameter
 	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString());
+		
 		// Tell controlled tank to aim at this point
+		GetControlledTank()->AimAt(HitLocation);
 	}
 }
 
